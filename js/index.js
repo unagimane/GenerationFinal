32ef66fa34a3c6f0e0e998929daf5b93aed5aee1
Abitii <aasmero129@gmail.com>
2022-11-07 12:56:49 -0600

    making changes to be able to add new element continuesly

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import ItemController from "./itemsController.js";
 
 //id generator
 let idGen = (Math.floor(Math.random() * 2));
-let item = new ItemController()
+let item = new ItemController();
 
 
 //input from html
@@ -16,24 +16,75 @@ const img = document.getElementById('imgurl');
 const btn = document.getElementById('button');
 
 //display data
-const displayTitle = document.getElementById('displaytitle');
-const displayDescription = document.getElementById('displaydescription');
-const displayImage = document.getElementById('image');
-const postDate = document.getElementById('time');
-document.getElementById('emptyCard').hidden = true;
+// const displayTitle = document.getElementById('displaytitle');
+// const displayDescription = document.getElementById('displaydescription');
+// const displayImage = document.getElementById('image');
+// const postDate = document.getElementById('time');
+// document.getElementById('emptyCard').hidden = true;
 
 //create instance
 const addItem = () => {
     let item = new ItemController(currentId++, name.value, description.value, img.value);
-    displayImage.src = `${img.value}`;
-    displayTitle.innerHTML = `${name.value}`;
-    displayDescription.innerHTML = `${description.value}`;
-    postDate.innerHTML = Date();
-    console.log(item);
-    document.getElementById('emptyCard').hidden = false;
+    if(name.vale === "" || description.value === ""  || img.value === ""){
+        alert("Please enter your information.");
+      }else{
+    const container = document.createElement("div");
+    const containerAttribute = document.createAttribute("class");
+    containerAttribute.value = "imageContainer";
+    container.setAttributeNode(containerAttribute);
+    const newImg = document.createElement("img")
+    newImg.src = `${img.value}`;
+    newImg.style.width = "631px";
+    newImg.style.height = "631px";
+    container.appendChild(newImg);
+    const nameElement = document.createElement("h5");
+    nameElement.textContent = `${name.value}`;
+    container.appendChild(nameElement)
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = `${description.value}`;
+    container.appendChild(descriptionElement);
+
+    const footerElement = document.createElement("footer");
+    const postElement = document.createElement("small");
+    // const minutes = new Date();
+    let minutes = 0;
+    setInterval(function(){
+        minutes++
+        if(minutes > 60){
+            postElement.innerHTML = `Last update ${minutes} minutes ago`;
+        }else{
+            postElement.innerHTML = `Last update 0 minutes ago`;
+        }
+         
+    },60000)
+    // const newMinutes = minutes.getMinutes();
+    postElement.textContent = `Last update ${minutes} minutes ago`;
+    
+    footerElement.appendChild(postElement);
+
+    container.appendChild(footerElement);
+
+    const postBody = document.getElementById("emptyCard");
+    postBody.appendChild(container);
+
+
+    
+    // displayImage.src = `${img.value}`;
+    // displayTitle.innerHTML = `${name.value}`;
+    // displayDescription.innerHTML = `${description.value}`;
+    // postDate.innerHTML = Date();
+    // console.log(item);
+    // document.getElementById('emptyCard').hidden = false;
+
+
+    
+
+    
 
     var form = document.querySelector('#input');
     form.reset();
+}
 
 }
 
